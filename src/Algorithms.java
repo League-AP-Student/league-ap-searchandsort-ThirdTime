0f4b7965fb3fbaa4c5524ef7650c26de78f5051d
Esme <esmepuzio@gmail.com>
2020-03-28 10:54:38 -0700

    Completed Search and Sort (with errors)

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,4 +1,109 @@
+import java.util.List;
 
 public class Algorithms {
+
+	public static int findBrokenEgg(List<String> eggs) {
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).equals("cracked")) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int countPearls(List<Boolean> oysters) {
+		int totalPearls = 0;
+		for (Boolean oyster : oysters) {
+			if (oyster.equals(true)) {
+				totalPearls++;
+			}
+		}
+		return totalPearls;
+	}
+
+	public static double findTallest(List<Double> heights) {
+		double tallest = -1;
+		for (Double height : heights) {
+			if (height > tallest) {
+				tallest = height;
+			}
+		}
+		return tallest;
+	}
+
+	public static String findLongestWord(List<String> words) {
+		String longestWord = "";
+		for (String word : words) {
+			if (word.length() > longestWord.length()) {
+				longestWord = word;
+			}
+		}
+		return longestWord;
+	}
+
+	public static boolean containsSOS(List<String> messages) {
+		for (String message : messages) {
+			if (message.contains("... --- ...")) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+
+	public static void sortScores(List<Double> examScores) {
+		for (int repetitionNum = 0; repetitionNum < examScores.size(); repetitionNum++) {
+			double lowestNum = 9999999;
+			int lowestNumIndex = 0;
+			for (int i = repetitionNum; i < examScores.size(); i++) {
+				if (examScores.get(i) < lowestNum) {
+					lowestNum = examScores.get(i);
+					lowestNumIndex = i;
+				}
+			}
+			Double tempVal = examScores.get(repetitionNum);
+			examScores.set(repetitionNum, lowestNum);
+			examScores.set(lowestNumIndex, tempVal);
+		}
+	}
+
+	public void sortDNA(List<String> unsortedSequences) {
+		for (int i = 0; i < unsortedSequences.size(); i++) {
+			int shortestLength = 9999999;
+			int shortestLengthIndex = 0;
+			for (int j = i; j < unsortedSequences.size(); i++) {
+				if(unsortedSequences.get(j).length() < shortestLength) {
+					shortestLength = unsortedSequences.get(j).length();
+				}
+			}
+			String tempVal = unsortedSequences.get(i);
+			unsortedSequences.set(i, unsortedSequences.get(shortestLengthIndex));
+			unsortedSequences.set(shortestLengthIndex, tempVal);
+		}
+	}
+	
+	public void testSortStringContents(List<String> words) {
+		
+			for (int i = 0; i < array.length - 1; i++) {
+				for (int j = i; j > 0; j--) {
+					if (array[j] < array[j - 1]) {
+						int tempNum = array[j];
+						array[j] = array[j - 1];
+						array[j - 1] = tempNum;
+						display.updateDisplay();
+					}
+				}
+			}
+			
+		for(int i = 0; i < words.size() - 1; i++) {
+			for(int j = i; j > 0; j--) {
+				if(words.get(j).compareTo(words.get(j-1)) < 0) {
+					String tempVal = words.get(j);
+					words.set(j, words.get(j - 1));
+					words.set(j - 1, tempVal);
+				}
+			}
+		}
+	}
 
 }
